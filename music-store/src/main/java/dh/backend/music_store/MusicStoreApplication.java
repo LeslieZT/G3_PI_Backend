956f7a70b9700b3a7ff42ddd94927d2d9dc4570a
LeslieZT <lfzarate@gmail.com>
2025-02-17 23:34:24 -0500

    initial setup

---
+++ music-store/src/main/java/dh/backend/music_store/MusicStoreApplication.java
@@ -0,0 +1,13 @@
+package dh.backend.music_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStoreApplication.class, args);
+	}
+
+}
